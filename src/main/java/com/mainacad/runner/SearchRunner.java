627c3ec8d8cc468f797142d2e60ab5d41c23e9d0
Antoninadan <mail100@i.ua>
2020-03-19 14:52:08 +0200

    redesign search, need fix

--- src/main/java/com/mainacad/runner/SearchRunner.java
+++ src/main/java/com/mainacad/runner/SearchRunner.java
@@ -1,100 +1,48 @@
 package com.mainacad.runner;
 
 import com.mainacad.service.SearchService;
+import com.mainacad.service.SearchThread;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class SearchRunner {
     List<Integer> listOfTwos = Collections.synchronizedList(new ArrayList<>());
 
     public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(21, 12, 3, 14, 25, 6, 7, 28, 9, 210, 112, 12, 213,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7, 28, 9,
-                210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 6, 7,
-                28, 9, 210, 112, 12, 213, 14, 15, 216, 17, 182, 19, 20, 8, 9);
-//        21, 12, 25,  28, 210, 112, 12, 213, 216, 182, 20
+        List<Integer> list = Arrays.asList(21, 12, 3, 14, 25, 1, 7, 28, 9, 210, 112, 12, 213,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 61, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 62, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 63, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 64, 3, 14, 25, 1, 7,
+                14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7, 28, 9,
+                210, 112, 12, 213, 14, 15, 5, 17, 182, 19, 20, 8, 9, 21, 12, 3, 14, 25, 1, 7,
+                28, 9, 210, 112, 12, 65, 14, 15, 5, 17, 182, 19, 20, 8, 9);
 
-        List<Integer> resultList = Collections.synchronizedList(new ArrayList<Integer>());
-
-        int size = list.size();
-        int parts = 3; // on what parts we can devide
-        int[][] result = getIndexesOfParts(size, parts);
-
-        System.out.println("size = " + size);
-        System.out.println("parts = " + parts);
-
+        // BUGS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        List<Integer> resultList;
         long timeStart1 = System.currentTimeMillis();
-        for (int i = 0; i < parts; i++) {
-            List<Integer> listForSearch = new ArrayList<>();
-            for (int j = result[i][0]; j <= result[i][1]; j++) {
-                listForSearch.add(list.get(j));
-            }
-            SearchService searchService = new SearchService(listForSearch, resultList);
-            searchService.start();
-        }
+        resultList = SearchService.search(list, 1);
         long timeEnd1 = System.currentTimeMillis();
         System.out.println((timeEnd1 - timeStart1) + " ms" + "- Multi method");
         System.out.println(resultList);
 
-        parts = 1; // on what parts we can devide
         long timeStart2 = System.currentTimeMillis();
-        for (int i = 0; i < parts; i++) {
-            List<Integer> listForSearch = new ArrayList<>();
-            for (int j = result[i][0]; j <= result[i][1]; j++) {
-                listForSearch.add(list.get(j));
-            }
-            SearchService searchService = new SearchService(listForSearch, resultList);
-            searchService.start();
-        }
+        resultList = SearchService.search(list, 5);
         long timeEnd2 = System.currentTimeMillis();
         System.out.println((timeEnd2 - timeStart2) + " ms" + "- Multi method");
         System.out.println(resultList);
-
-
-    }
-
-
-    private static int[][] getIndexesOfParts(int size, int parts) {
-        int[][] result = new int[parts][2];
-        int remains = getRemainsOfDividing(size, parts);
-        int m = 0;
-
-        for (int i = 1; i <= parts; i++) {
-            if (m != (parts - 1)) {
-                result[i - 1][0] = m * (size / parts);
-                result[i - 1][1] = ((m + 1) * (size / parts) - 1);
-            } else {
-                result[i - 1][0] = m * (size / parts);
-                result[i - 1][1] = (m + 1) * (size / parts) + remains - 1;
-            }
-            m++;
-        }
-        return result;
-    }
-
-    private static int getRemainsOfDividing(int size, int parts) {
-        if (parts * (size / parts) == size) {
-            return 0;
-        } else {
-            return size - parts * (size / parts);
-        }
     }
 }
--- src/main/java/com/mainacad/service/SearchService.java
+++ src/main/java/com/mainacad/service/SearchService.java
@@ -1,27 +1,61 @@
 package com.mainacad.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class SearchService extends Thread {
-    List<Integer> listForSearch;
-    List<Integer> resultList;
+public class SearchService {
+    public static List<Integer> resultList =  Collections.synchronizedList(new ArrayList<>());
 
+    public static List<Integer> search(List<Integer> Integers, int parts) {
+        List<List<Integer>> listOfLists = divideList(Integers, parts);
 
-    public SearchService(List<Integer> listForSearch, List<Integer> resultList) {
-        this.listForSearch = listForSearch;
-        this.resultList = resultList;
+        for (List<Integer> listForSearch:listOfLists) {
+            SearchThread SearchThread = new SearchThread(listForSearch);
+            SearchThread.start();
+        }
+        return resultList;
     }
 
-    @Override
-    public void run() {
-        resultList.addAll(search());
+    public static <T> List<List<T>> divideList(List<T> list, int parts) {
+        int size = list.size();
+        int[][] indexesOfParts = getIndexesOfParts(size, parts);
+        List<List<T>> result = new ArrayList<>();
+
+        for (int i = 0; i < parts; i++) {
+            List<T> listForSearch = new ArrayList<>();
+            for (int j = indexesOfParts[i][0]; j <= indexesOfParts[i][1]; j++) {
+                listForSearch.add(list.get(j));
+            }
+            result.add(listForSearch);
+        }
+        return result;
     }
 
-    private List<Integer> search() {
-        return listForSearch.stream().filter(it -> Integer.toString(it).contains("2")).collect(Collectors.toList());
+    public static int[][] getIndexesOfParts(int size, int parts) {
+        int[][] result = new int[parts][2];
+        int remains = getRemainsOfDividing(size, parts);
+        int m = 0;
+
+        for (int i = 1; i <= parts; i++) {
+            if (m != (parts - 1)) {
+                result[i - 1][0] = m * (size / parts);
+                result[i - 1][1] = ((m + 1) * (size / parts) - 1);
+            } else {
+                result[i - 1][0] = m * (size / parts);
+                result[i - 1][1] = (m + 1) * (size / parts) + remains - 1;
+            }
+            m++;
+        }
+        return result;
+    }
+
+    public static int getRemainsOfDividing(int size, int parts) {
+        if (parts * (size / parts) == size) {
+            return 0;
+        } else {
+            return size - parts * (size / parts);
+        }
     }
 }
+
